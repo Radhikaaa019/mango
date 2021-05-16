a2ba4f3294b3ae73162a520df5cf6ed5a38d5163
Radhikaaa019 <78020489+Radhikaaa019@users.noreply.github.com>
2021-05-16 09:35:43 +0530

    Add files via upload

---
+++ Elastic.js
@@ -0,0 +1,27 @@
+class Elastic{
+constructor(bodyA,pointB){
+    var options = {
+        bodyA : bodyA,
+        pointB: pointB,
+        stiffness : 0.04,
+        length: 10
+    }
+    this.pointB= pointB
+    this.elastic = Constraint.create(options);
+    World.add(world, this.elastic);
+}
+attached(body){
+    this.elastic.bodyA=body
+}
+fly (){
+    this.elastic.bodyA=null
+}
+display(){
+    if(this.elastic.bodyA){
+        var pointA = this.elastic.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
+}
